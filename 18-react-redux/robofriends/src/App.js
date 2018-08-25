6f76192fc42782e974018645870ec0ebff42b3f6
Jeremy Bell <jbell0385@gmail.com>
2018-08-25 12:53:15 -0700

    react progress

---
+++ 18-react-redux/robofriends/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import CardList from './CardList'
+import SearchBox from './SearchBox'
+import { robots } from './robots'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    console.log(filteredRobots);
+  }
+
+  render() {
+    return (
+      <div className='tc' >
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />
+      </div>
+    )
+  }
+}
+
+export default App;
+
